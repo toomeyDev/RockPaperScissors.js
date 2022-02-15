78e59681f5cc4018176459078c5ddd305e9f18ec
Matthew Toomey <mtoomeydev@gmail.com>
2022-02-15 14:19:58 -0500

    implement score tracking

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+// initialize both scores to 0 at start
+let playerScore = 0, cpuScore = 0;
+
 function computerPlay() {
     let cpuGuess = Math.floor(Math.random()*3);
     switch(cpuGuess) {
@@ -12,22 +15,54 @@ function computerPlay() {
     }
 }
 
+// handle adjustment of scores for player and computer
+function adjustScore(gameOutcome) {
+    switch(gameOutcome) {
+        case "win":
+            playerScore++;
+            break;
+        case "lose":
+            cpuScore++;
+            break;
+        default:
+            break;
+    }
+}
+
 function playRound(playerChoice=computerPlay()) {
     let cpuChoice = computerPlay();
     switch(playerChoice) {
         case "rock":
             if(cpuChoice == "rock") return "tie";
-            if(cpuChoice == "paper") return "lose";
-            if(cpuChoice == "scissors") return "win";
+            if(cpuChoice == "paper"){
+                adjustScore('lose');
+                return "lose";
+            }
+            if(cpuChoice == "scissors"){
+                adjustScore('win');
+                return "win";
+            }
             break;
         case "paper":
-            if(cpuChoice == "rock") return "win";
+            if(cpuChoice == "rock"){
+                adjustScore('win');
+                return "win";
+            }
             if(cpuChoice == "paper") return "tie";
-            if(cpuChoice == "scissors") return "lose";
+            if(cpuChoice == "scissors"){
+                adjustScore('lose');
+                return "lose";
+            }
             break;
         case "scissors":
-            if(cpuChoice == "rock") return "lose";
-            if(cpuChoice == "paper") return "win";
+            if(cpuChoice == "rock"){
+                adjustScore('lose');
+                return "lose";
+            }
+            if(cpuChoice == "paper"){
+                adjustScore('win');
+                return "win";
+            }
             if(cpuChoice == "scissors") return "tie";
             break;
         default:
@@ -59,29 +94,21 @@ function game() {
         // then play a single round with that selection
         rockBtn.addEventListener('click', function () {
             result.textContent = playRound('rock');
+            runningTotal.textContent = `Current score:\n 
+                P ${playerScore} / CPU ${cpuScore}`;
         });
     
         paperBtn.addEventListener('click', function() {
             result.textContent = playRound('paper');
+            runningTotal.textContent = `Current score:\n 
+                P ${playerScore} / CPU ${cpuScore}`;
         });
         
         scissorsBtn.addEventListener('click', function() {
             result.textContent = playRound('scissors');
+            runningTotal.textContent = `Current score:\n 
+                P ${playerScore} / CPU ${cpuScore}`;
         });
-
-    // handle adjustment of scores for player and computer
-    function adjustScore(gameOutcome) {
-        switch(gameOutcome) {
-            case "win":
-                playerScore++;
-                break;
-            case "lose":
-                cpuScore++;
-                break;
-            default:
-                break;
-        }
-    }
 
     function getWinner() {
         if(playerScore === cpuScore) return "Game ends in a tie!";
@@ -89,9 +116,6 @@ function game() {
         if(playerScore > cpuScore) return "Player wins the game!";
     }
 
-    let playerScore = 0, cpuScore = 0;
-
-        let cpuChoice = computerPlay();
 }
 
 game()
